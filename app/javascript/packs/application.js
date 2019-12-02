dcefefa114d3dc4e23f83bcf49a2d2923dd5bd45
Bruno Calmels <bruno.calmels@macherit.com>
2019-12-02 12:00:09 -0300

    chore: Configuracion de archivos para compilacion de assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,24 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// base dependency library, it should be only shared by `admin.js` and `application.js`.
+import "jquery";
+import "bootstrap/dist/js/bootstrap";
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import RailsUjs from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
+const images = require.context("images", true);
+const imagePath = name => images(name, true);
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+RailsUjs.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-require("trix")
-require("@rails/actiontext")
+$(document).on("turbolinks:load", function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+require.context("../images", true);
+
+import "./bootstrap_custom.js";
+
+import "styles/application";
