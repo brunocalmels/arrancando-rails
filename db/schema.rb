19a18e4d8a55066364602accb65b529c7b44163c
Bruno Calmels <bruno.calmels@macherit.com>
2020-03-02 14:08:24 -0300

    feat: Ranking de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_213410) do
+ActiveRecord::Schema.define(version: 2020_03_02_164725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_213410) do
     t.boolean "activo"
     t.string "usernames_pasados", default: [], array: true
     t.datetime "last_seen_at"
+    t.integer "rank"
     t.index ["email"], name: "index_users_on_email"
     t.index ["username"], name: "index_users_on_username"
   end
