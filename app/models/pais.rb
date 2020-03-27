ba549565bbfaae58a76c169285b1fc2f47aae891
ivan eidel <ivan.eidelstein@gmail.com>
2020-03-27 17:09:50 -0300

    chore: Annotate

--- app/models/pais.rb
+++ app/models/pais.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: paises
+#
+#  id         :integer          not null, primary key
+#  nombre     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Pais < ApplicationRecord
   has_many :provincias
 
