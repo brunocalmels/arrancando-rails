fd1af2bb90565fadd1690376262435c6a8b4b846
Bruno Calmels <bruno.calmels@macherit.com>
2020-04-16 18:55:18 -0300

    Revert "chore: Versión 1.2.6+42"
    This reverts commit 9ba04d4b9dc1b0fef8f6206b84ac832c5d49c84d.

--- config/initializers/constantes.rb
+++ config/initializers/constantes.rb
@@ -60,4 +60,4 @@ ARRANCANDO_URL = "https://arrancando.com.ar".freeze
 ANDROID_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=com.macherit.arrancando".freeze
 IOS_DOWNLOAD_URL = "https://apps.apple.com/us/app/arrancando/id1490590335?l=es".freeze
 
-APP_VERSION = "1.2.6+42".freeze
+APP_VERSION = "1.2.6+41".freeze
