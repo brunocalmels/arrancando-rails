15836107bb411dfc4362d1dce81eb958490f45b7
ivan eidel <ivan.eidelstein@gmail.com>
2020-06-04 10:10:13 -0300

    feat: Ordenar por puntaje, por cantidad de votos y por cantidad de comentarios (publicaciones y recetas)

--- ORDER_PUBLICACIONES.sql
+++ ORDER_PUBLICACIONES.sql
@@ -1,7 +1,7 @@
-SELECT publicaciones.id, avg, coms
+SELECT publicaciones.id, avg, cant_punt, coms
 from publicaciones LEFT JOIN 
   (
-    SELECT id, avg(value::FLOAT)
+    SELECT id, avg(value::FLOAT), count(*) as cant_punt
     FROM "publicaciones" LEFT JOIN jsonb_each(puntajes) d
     ON true GROUP BY "publicaciones"."id"
   ) complex ON publicaciones.id = complex.id
@@ -10,4 +10,4 @@ from publicaciones LEFT JOIN
     FROM comentario_publicaciones
     GROUP BY publicacion_id
   ) comms_pub ON publicaciones.id = comms_pub.publicacion_id
-ORDER BY avg DESC nulls LAST, coms DESC nulls LAST
+ORDER BY avg DESC nulls LAST, cant_punt DESC nulls LAST, coms DESC nulls LAST;
--- app/models/receta.rb
+++ app/models/receta.rb
@@ -79,7 +79,7 @@ class Receta < ApplicationRecord
       order(recetas[:updated_at].send(direction))
     when "puntuacion"
       # rubocop:disable Metrics/LineLength
-      q = 'SELECT recetas.id, avg, coms from recetas LEFT JOIN (SELECT id, avg(value::FLOAT) FROM "recetas" LEFT JOIN jsonb_each(puntajes) d ON true GROUP BY "recetas"."id") complex ON recetas.id = complex.id LEFT JOIN (SELECT receta_id, count(*) AS coms FROM comentario_recetas GROUP BY receta_id) comms_rec ON recetas.id = comms_rec.receta_id ORDER BY avg DESC nulls LAST, coms DESC nulls LAST'
+      q = 'SELECT recetas.id, avg, cant_punt, coms from recetas LEFT JOIN  ( SELECT id, avg(value::FLOAT), count(*) as cant_punt FROM "recetas" LEFT JOIN jsonb_each(puntajes) d ON true GROUP BY "recetas"."id" ) complex ON recetas.id = complex.id LEFT JOIN ( SELECT receta_id, count(*) AS coms FROM comentario_recetas GROUP BY receta_id ) comms_pub ON recetas.id = comms_pub.receta_id ORDER BY avg DESC nulls LAST, cant_punt DESC nulls LAST, coms DESC nulls LAST'
       ids = ActiveRecord::Base.connection.execute(q).pluck "id"
       Receta.where(id: ids).order_by_ids(ids)
       # rubocop:enable Metrics/LineLength
