a493c52fe53ad432d5801277046e1c155926a8ab
Bruno Calmels <bruno.calmels@macherit.com>
2020-11-24 17:03:51 -0300

    feat: Sort por fecha de actualizacion y creacion de publicaciones y recetas

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -73,6 +73,8 @@ class Publicacion < ApplicationRecord
     case sort_option.to_s
     when "fecha"
       order(publicaciones[:updated_at].send(direction))
+    when "fecha_creacion"
+      order(publicaciones[:created_at].send(direction))
     when "puntuacion"
       # rubocop:disable Metrics/LineLength
       q = 'SELECT publicaciones.id, avg, cant_punt, coms from publicaciones LEFT JOIN  ( SELECT id, avg(value::FLOAT), count(*) as cant_punt FROM "publicaciones" LEFT JOIN jsonb_each(puntajes) d ON true GROUP BY "publicaciones"."id" ) complex ON publicaciones.id = complex.id LEFT JOIN ( SELECT publicacion_id, count(*) AS coms FROM comentario_publicaciones GROUP BY publicacion_id ) comms_pub ON publicaciones.id = comms_pub.publicacion_id ORDER BY avg DESC nulls LAST, cant_punt DESC nulls LAST, coms DESC nulls LAST'
