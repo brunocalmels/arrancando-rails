6c577b27029086c2feca2ae5310e416eda3e439f
Bruno Calmels <bruno.calmels@macherit.com>
2020-12-29 12:49:50 -0300

    feat: Recibe mensajes nuevos de app por WS

--- app/controllers/mensaje_chats_controller.rb
+++ app/controllers/mensaje_chats_controller.rb
@@ -13,29 +13,4 @@ class MensajeChatsController < ApplicationController
       render json: @mensaje_chat.errors, status: :unprocessable_entity
     end
   end
-
-  private
-
-  def mensaje_chat_params
-    params.require(:mensaje_chat).permit(:mensaje, :grupo_chat_id)
-  end
-
-  def to_broadcast(msj)
-    user = msj.user
-    has_avatar = user.avatar.attached?
-    avatar = has_avatar ? rails_blob_path(user.avatar) : "/images/unknown.png"
-
-    Hash(
-      id: msj.id,
-      grupo_chat_id: msj.grupo_chat.id,
-      usuario: {
-        id: user.id,
-        avatar: avatar,
-        username: user.username
-      },
-      created_at: msj.created_at,
-      updated_at: msj.updated_at,
-      mensaje: msj.mensaje
-    )
-  end
 end
--- app/helpers/mensaje_chats_helper.rb
+++ app/helpers/mensaje_chats_helper.rb
@@ -1,2 +1,33 @@
 module MensajeChatsHelper
+  include ContentHelper
+
+  def to_broadcast(msj)
+    user = msj.user
+    has_avatar = user.avatar.attached?
+    avatar = if has_avatar
+               Rails.application.routes.url_helpers.rails_blob_path(
+                 user.avatar,
+                 only_path: true
+               )
+             else
+               "/images/unknown.png"
+             end
+
+    Hash(
+      id: msj.id,
+      grupo_chat_id: msj.grupo_chat.id,
+      usuario: {
+        id: user.id,
+        avatar: avatar,
+        username: user.username
+      },
+      created_at: msj.created_at,
+      updated_at: msj.updated_at,
+      mensaje: msj.mensaje
+    )
+  end
+
+  def mensaje_chat_params
+    params.require(:mensaje_chat).permit(:mensaje, :grupo_chat_id)
+  end
 end
