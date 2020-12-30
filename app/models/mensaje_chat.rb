64b6fd20d11ce103a36b5b88eb2dff17da917f6e
Bruno Calmels <bruno.calmels@macherit.com>
2020-12-30 10:28:12 -0300

    feat: Max length a MensajeChat

--- app/models/mensaje_chat.rb
+++ app/models/mensaje_chat.rb
@@ -2,7 +2,7 @@ class MensajeChat < ApplicationRecord
   belongs_to :grupo_chat, inverse_of: :mensaje_chats
   belongs_to :user
 
-  validates :mensaje, presence: true
+  validates :mensaje, presence: true, length: { minimum: 1, maximum: 5000 }
 
   scope :last_first, -> { order(created_at: :desc) }
 
